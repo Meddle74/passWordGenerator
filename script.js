71ec59271618de223ec5363d61c561df6297e6d5
Patrick <patrick.lloyd@theproducebox.com>
2020-09-19 17:06:48 -0400

    finished logic for prompts

--- script.js
+++ script.js
@@ -1,28 +1,78 @@
 // Assignment Code
 var generateBtn = document.querySelector('#generate');
 
+var includeUpperCase = '';
+var includeLowerCase = '';
+var includeNumbers = '';
+var includeSymbols = '';
+var finalPass = '';
+var password_length = '';
+
 function generatePassword() {
+  
+  var password_length = prompt(
+    'How many characters would you like your password to contain?'
+  );
 
+  // check to verify password is a number
+  while (isNaN(password_length)) {
+    alert('Password length must be a number');
+    var password_length = prompt(
+      'How many characters would you like your password to contain?'
+    );
+  }
+  
+  //check to make sure password_length is the correct length and then also verify again if it a number in case it gets out of loop
+  while (password_length <= 7 || password_length >= 129) {
+    alert('Password must be between 8 and 128 characthers in length');
+    var password_length = prompt(
+      'How many characters would you like your password to contain?'
+    );
+    while (isNaN(password_length)) {
+      alert('Password length must be a number');
+      var password_length = prompt(
+        'How many characters would you like your password to contain?'
+      );
+    }
+  }
 
-  /*
-testing section remove for final code push
-*/
+  //prompts to select items in password
+  var includeUpperCase = confirm(
+    'Click ok of you want your password to include uppercase letters'
+  );
+  var includeLowerCase = confirm(
+    'Click ok of you want your password to include lowercase letters'
+  );
+  var includeNumbers = confirm(
+    'Click ok of you want your password to include numbers'
+  );
+  var includeSymbols = confirm(
+    'Click ok of you want your password to include symbols'
+  );
 
-  includeUpperCase = 1;
-  includeLowerCase = 0;
-  includeNumbers = 1;
-  includeSymbols = 1;
-
-  charactherLength = 9;
-
-  /*
-testing section remove for final code push
-*/
-
-  finalPass = '';
+  // make sure one selection has been confirmed
+  while (
+    includeUpperCase === false &&
+    includeLowerCase === false &&
+    includeNumbers === false &&
+    includeSymbols === false
+  ) {
+    alert('You must choose at least one parameter');
+    var includeUpperCase = confirm(
+      'Click ok of you want your password to include uppercase letters'
+    );
+    var includeLowerCase = confirm(
+      'Click ok of you want your password to include lowercase letters'
+    );
+    var includeNumbers = confirm(
+      'Click ok of you want your password to include numbers'
+    );
+    var includeSymbols = confirm(
+      'Click ok of you want your password to include symbols'
+    );
+  }
 
   var characterObject = {
-
     getUpperCase: function () {
       return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
     },
@@ -68,8 +118,8 @@ testing section remove for final code push
     },
   };
 
-  for (var i = 0; i < charactherLength; i++) {
-    finalPass = finalPass + characterObject.getAllChar()[ Math.floor(Math.random() * characterObject.getAllChar().length)];
+  for (var i = 0; i < password_length; i++) {
+    finalPass = finalPass + characterObject.getAllChar()[Math.floor(Math.random() * characterObject.getAllChar().length)];
   }
   return finalPass;
 }
